3adc1bc817fa14e2e915cbb4a2747630411b3a97
lsvard <l9244492@bk.ru>
2023-08-08 17:18:46 +0300

    Done

--- HW2.2 TrafficLight/ViewController.swift
+++ HW2.2 TrafficLight/ViewController.swift
@@ -24,62 +24,62 @@ class ViewController: UIViewController {
     }
     
     @IBAction func startButtonDidTapped() {
-            if redView.alpha != 1 && yellowView.alpha != 1 && greenView.alpha != 1 {
-                redView.alpha = 1
-                startButton.setTitle("NEXT", for: .normal)
-            } else if redView.alpha == 1 {
-                redView.alpha = 0.3
-                yellowView.alpha = 1
-            } else if yellowView.alpha == 1 {
-                yellowView.alpha = 0.3
-                greenView.alpha = 1
-            } else if greenView.alpha == 1 {
-                greenView.alpha = 0.3
-                redView.alpha = 1
-            }
+        if redView.alpha != 1 && yellowView.alpha != 1 && greenView.alpha != 1 {
+            redView.alpha = 1
+            startButton.setTitle("NEXT", for: .normal)
+        } else if redView.alpha == 1 {
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+        } else if yellowView.alpha == 1 {
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        } else if greenView.alpha == 1 {
+            greenView.alpha = 0.3
+            redView.alpha = 1
         }
-  
-//    @IBAction func startButtonDidTapped(_ light: Double) {
-//        let alpha = 0.0
-//        switch alpha {
-//        case redView.alpha:
-//            redView.alpha = 1.0
-//            yellowView.alpha = 0.3
-//            greenView.alpha = 0.3
-//            fallthrough
-//        case yellowView.alpha:
-//            redView.alpha = 0.3
-//            greenView.alpha = 0.3
-//            yellowView.alpha = 1
-//            fallthrough
-//        case greenView.alpha:
-//            redView.alpha = 0.3
-//            yellowView.alpha = 0.3
-//            greenView.alpha = 1
-//        default:
-//            break
-//        }
-//    }
+    }
     
-//    @IBAction func startButtonDidTapped(_ light: Double) {
-//        switch light {
-//        case redView.alpha:
-//            redView.alpha = 1.0
-//            yellowView.alpha = 0.3
-//            greenView.alpha = 0.3
-//            startButton.setTitle("NEXT", for: .normal)
-//            fallthrough
-//        case yellowView.alpha:
-//            redView.alpha = 0.3
-//            greenView.alpha = 0.3
-//            yellowView.alpha = 1
-//            fallthrough
-//        case greenView.alpha:
-//            redView.alpha = 0.3
-//            yellowView.alpha = 0.3
-//            greenView.alpha = 1
-//        default:
-//            break
-//        }
-//    }
+    //    @IBAction func startButtonDidTapped(_ light: Double) {
+    //        let alpha = 0.0
+    //        switch alpha {
+    //        case redView.alpha:
+    //            redView.alpha = 1.0
+    //            yellowView.alpha = 0.3
+    //            greenView.alpha = 0.3
+    //            fallthrough
+    //        case yellowView.alpha:
+    //            redView.alpha = 0.3
+    //            greenView.alpha = 0.3
+    //            yellowView.alpha = 1
+    //            fallthrough
+    //        case greenView.alpha:
+    //            redView.alpha = 0.3
+    //            yellowView.alpha = 0.3
+    //            greenView.alpha = 1
+    //        default:
+    //            break
+    //        }
+    //    }
+    
+    //    @IBAction func startButtonDidTapped(_ light: Double) {
+    //        switch light {
+    //        case redView.alpha:
+    //            redView.alpha = 1.0
+    //            yellowView.alpha = 0.3
+    //            greenView.alpha = 0.3
+    //            startButton.setTitle("NEXT", for: .normal)
+    //            fallthrough
+    //        case yellowView.alpha:
+    //            redView.alpha = 0.3
+    //            greenView.alpha = 0.3
+    //            yellowView.alpha = 1
+    //            fallthrough
+    //        case greenView.alpha:
+    //            redView.alpha = 0.3
+    //            yellowView.alpha = 0.3
+    //            greenView.alpha = 1
+    //        default:
+    //            break
+    //        }
+    //    }
 }
