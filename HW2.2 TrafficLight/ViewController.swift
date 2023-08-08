dc2b548b9717d0d17002a5b9f159b22615d3da75
lsvard <l9244492@bk.ru>
2023-08-08 10:11:41 +0300

    Work with logic

--- HW2.2 TrafficLight/ViewController.swift
+++ HW2.2 TrafficLight/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var orangeView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.alpha = 0.3
+        orangeView.alpha = 0.3
+        greenView.alpha = 0.3
+        startButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func startButtonDidTapped() {
+        redView.alpha = 1
+        if redView.alpha == 1 {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+    }
 }
-
