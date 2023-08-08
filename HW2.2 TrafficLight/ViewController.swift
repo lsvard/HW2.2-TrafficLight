2b2ea922dd54b84d06569379dbaca39fcc18831a
lsvard <l9244492@bk.ru>
2023-08-08 12:59:09 +0300

    Work with logic

--- HW2.2 TrafficLight/ViewController.swift
+++ HW2.2 TrafficLight/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     @IBOutlet var redView: UIView!
     @IBOutlet var orangeView: UIView!
     @IBOutlet var greenView: UIView!
-    
     @IBOutlet var startButton: UIButton!
     
     override func viewDidLoad() {
@@ -23,21 +22,37 @@ class ViewController: UIViewController {
         startButton.layer.cornerRadius = 10
     }
     
-    @IBAction func startButtonDidTapped() {
-        startButton.setTitle("NEXT", for: .normal)
-        if redView.alpha != 1 {
+    //    @IBAction func startButtonDidTapped() {
+    //        startButton.setTitle("NEXT", for: .normal)
+    //        if redView.alpha != 1 {
+    //            redView.alpha = 1
+    //            orangeView.alpha = 0.3
+    //            greenView.alpha = 0.3
+    //        } else if orangeView.alpha != 1 {
+    //            orangeView.alpha = 1
+    //            redView.alpha = 0.3
+    //            greenView.alpha = 0.3
+    //        } else if greenView.alpha != 1 {
+    //            greenView.alpha = 1
+    //            redView.alpha = 0.3
+    //            orangeView.alpha = 0.3
+    //        }
+    //    }
+    @IBAction func startButtonDidTapped(_ viewAlpha: Double) {
+        switch viewAlpha {
+        case redView.alpha:
             redView.alpha = 1
-            orangeView.alpha = 0.3
-            orangeView.alpha = 0.3
-        } else if orangeView.alpha != 1 {
+            fallthrough
+        case orangeView.alpha:
             redView.alpha = 0.3
-            greenView.alpha = 0.3
             orangeView.alpha = 1
-        } else if greenView.alpha != 1 {
+            fallthrough
+        case greenView.alpha:
             redView.alpha = 0.3
             orangeView.alpha = 0.3
             greenView.alpha = 1
+        default:
+            break
         }
-        
     }
 }
