79dc301cdc63e28df6dda8e52b4485423d50539b
lsvard <l9244492@bk.ru>
2023-08-09 20:56:38 +0300

    Optimisation

--- HW2.2 TrafficLight/ViewController.swift
+++ HW2.2 TrafficLight/ViewController.swift
@@ -24,16 +24,19 @@ class ViewController: UIViewController {
     }
     
     @IBAction func startButtonDidTapped() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+            
         if redView.alpha != 1 && yellowView.alpha != 1 && greenView.alpha != 1 {
             redView.alpha = 1
-            startButton.setTitle("NEXT", for: .normal)
         } else if redView.alpha == 1 {
             redView.alpha = 0.3
             yellowView.alpha = 1
         } else if yellowView.alpha == 1 {
             yellowView.alpha = 0.3
             greenView.alpha = 1
-        } else if greenView.alpha == 1 {
+        } else {
             greenView.alpha = 0.3
             redView.alpha = 1
         }
